1e7de8f14b4c26bca3f8fa913024f1970d76468b
Junwei Low <lowjunwei99@gmail.com>
2020-04-20 15:20:41 +0800

    step 1

--- app.py
+++ app.py
@@ -1,101 +1,139 @@
-from flask import Flask, request
-from flask_sqlalchemy import SQLAlchemy
-from flask_restful import Resource, Api
+from flask import Flask,request
 import os
-from flask_jwt import JWT, jwt_required
-from flask_migrate import Migrate
+import sqlite3
+from flask_restful import Resource, Api
+from flask_jwt import JWT,jwt_required
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "mysecretkey"
-basedir = os.path.abspath(os.path.dirname(__file__))
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(basedir,"data.sqlite")
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-db = SQLAlchemy(app)
-Migrate(app,db)
 
-class User(db.Model):
 
-    id = db.Column(db.Integer,primary_key=True)
-    username = db.Column(db.String(20),unique=True)
-    password = db.Column(db.String(20))
+############################################################
 
-    def __init__(self,username,password):
+
+def get_db():
+    db = sqlite3.connect("db.sqlite3")
+    db.row_factory = sqlite3.Row
+    return db
+
+def create_db():
+    db = get_db()
+    db.execute("CREATE TABLE posts " + \
+                "(id INTEGER PRIMARY KEY AUTOINCREMENT, " + \
+                "title TEXT NOT NULL, " + \
+                "content TEXT)")
+    db.execute("CREATE TABLE users" + \
+                "(id INTEGER PRIMARY KEY AUTOINCREMENT, " + \
+                "username TEXT NOT NULL, " + \
+                "password TEXT NOT NULL)")
+    db.commit()
+    db.close()
+
+
+if not os.path.isfile('db.sqlite3'):
+    create_db()
+
+def json_posts(post):
+    return {"id": post["id"], "title": post["title"], "content": post["content"]}
+
+class User:
+
+    def __init__(self,id,username,password):
+        self.id = id
         self.username = username
         self.password = password
+###########################################################
 
-class Post(db.Model):
-
-    id = db.Column(db.Integer,primary_key=True)
-    title = db.Column(db.String(30))
-    content = db.Column(db.String(400))
-
-    def __init__(self,title,content):
-        self.title = title
-        self.content = content
-
-    def json(self):
-        return {"id":self.id, "title":self.title, "content":self.content}
-
-db.create_all()
 
 def authenticate(username,password):
 
-    user = User.query.filter_by(username=username).first()
-    if user and password == user.password:
-        return user
+    db = get_db()
+    cursor = db.execute("SELECT * FROM users WHERE username = ?",(username,))
+    user = cursor.fetchone()
+    db.close()
+    if user and password == user["Password"]:
+        user_object = User(user["id"],user["username"],user["password"])
+        return user_object
 
 def identity(payload):
 
     user_id = payload["identity"]
-    return User.query.filter_by(id=user_id).first()
+    db = get_db()
+    cursor = db.execute("SELECT * FROM users WHERE id = ?",(user_id,))
+    user = cursor.fetchone()
+    db.close()
+    user_object = User(user["id"],user["username"],user["password"])
+    return user_object
+
 
 api = Api(app)
 jwt = JWT(app,authenticate,identity)
 
+###################################################
+
+
 class IndivPosts(Resource):
 
+    @jwt_required()
     def get(self,id):
 
-         post = Post.query.filter_by(id=id).first()
-         return post.json()
+        db = get_db()
+        cursor = db.execute("SELECT * FROM posts WHERE id = ?",(id,))
+        post = cursor.fetchone()
+        db.close()
+        return json_posts(post)
 
+
+    @jwt_required()
     def delete(self,id):
 
-        post = Post.query.filter_by(id=id).first()
-        db.session.delete(post)
-        db.session.commit()
-        return {"note":"delete successful"}
+        db = get_db()
+        cursor = db.execute("DELETE FROM posts WHERE id = ?",(id,))
+        db.commit()
+        db.close()
+        return {"note":"delete"}
 
+
+    @jwt_required()
     def put(self,id):
 
-        post = Post.query.filter_by(id=id).first()
         json_data = request.get_json(force=True)
-        title = json_data["title"]
-        content = json_data["content"]
-        post.title = title
-        post.content = content
-        db.session.commit()
+        db = get_db()
+        cursor = db.execute("UPDATE posts SET title=? WHERE id=?",(json_data["title"],id))
+        cursor = db.execute("UPDATE posts SET content=? WHERE id=?",(json_data["content"],id))
+        db.commit()
+        db.close()
         return {"note": "updated"}
+
 
 class CreatePosts(Resource):
 
+    @jwt_required()
     def post(self):
+
 
         json_data = request.get_json(force=True)
         title = json_data["title"]
         content = json_data["content"]
-        post = Post(title=title, content=content)
-        db.session.add(post)
-        db.session.commit()
-        return post.json()
+        db = get_db()
+        cursor = db.execute("INSERT INTO posts(title,content) VALUES(?,?)",(title,content))
+        db.commit()
+        db.close()
+        return {"note": "created"}
 
 class AllPosts(Resource):
 
     @jwt_required()
     def get(self):
 
-        posts = Post.query.all()
-        return [post.json() for post in posts]
+
+        db = get_db()
+        cursor = db.execute("SELECT * FROM posts")
+        posts = cursor.fetchall()
+        db.close()
+        return [json_posts(post) for post in posts]
+
+
 
 class NewUser(Resource):
 
@@ -104,15 +142,16 @@ class NewUser(Resource):
         json_data = request.get_json(force=True)
         new_username = json_data["username"]
         new_password = json_data["password"]
-        usernames = []
-        for u in User.query.all():
-            usernames.append(u.username)
+
+        db = get_db()
+        cursor = db.execute("SELECT username FROM users")
+        usernames = cursor.fetchall()
         if new_username in usernames:
-            return {"note":"user already exist"}
+            return {"note": "user already exist"}
         else:
-            newuser = User(username=new_username, password=new_password)
-            db.session.add(newuser)
-            db.session.commit()
+            cursor = db.execute("INSERT INTO users(username,password) VALUES (?,?)",(new_username,new_password))
+            db.commit()
+            db.close()
             return {"note":"new user created"}
 
 api.add_resource(IndivPosts,"/post/<int:id>")
